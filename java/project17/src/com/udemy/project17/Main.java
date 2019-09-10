f631dade00bbb13d1e34a07339dcb55e06887842
Azadprog <rehman19942008@hotmail.com>
2019-09-10 03:03:34 +0200

    editing the project 17

--- java/project17/src/com/udemy/project17/Main.java
+++ java/project17/src/com/udemy/project17/Main.java
@@ -3,5 +3,11 @@ package com.udemy.project17;
 public class Main {
     public static void main(String[] args) {
 
+
+MinutesToYearsDaysCalculator.printYearsAndDays(525600);
+MinutesToYearsDaysCalculator.printYearsAndDays(1051200);
+MinutesToYearsDaysCalculator.printYearsAndDays(561600);
+MinutesToYearsDaysCalculator.printYearsAndDays(0);
+
     }
 }
--- java/project17/src/com/udemy/project17/MinutesToYearsDaysCalculator.java
+++ java/project17/src/com/udemy/project17/MinutesToYearsDaysCalculator.java
@@ -1,10 +1,13 @@
 package com.udemy.project17;
 
 public class MinutesToYearsDaysCalculator {
+    public static void printYearsAndDays(long minutes) {
+        long hours = minutes / 60;
+        long day = hours / 24;
+        long year = day / 365;
+        long remainingDays = day % 365;
 
-    public static void printYearsAndDays (long minutes){
-
-
+        String  res = (minutes < 0) ? "invalid Value"  :  minutes + " min = " + year + " y " + " and " + remainingDays + " d";
+        System.out.println(res);
     }
-
 }
