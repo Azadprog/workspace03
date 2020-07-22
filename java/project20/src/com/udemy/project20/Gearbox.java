edc21359b238df03a563b3dad482fb753d25f726
Azad <22692378+Azadprog@users.noreply.github.com>
2020-07-22 16:27:04 +0200

    add

---
+++ java/project20/src/com/udemy/project20/Gearbox.java
@@ -0,0 +1,8 @@
+package com.udemy.project20;
+
+public class Gearbox{
+  private boolean clutchIsIn;
+  public void opeateClutch(boolean inOrOut){
+    this.clutchIsIn  = inOrOut;
+  }
+}
--- java/project20/src/com/udemy/project20/Main.java
+++ java/project20/src/com/udemy/project20/Main.java
@@ -2,7 +2,21 @@ package com.udemy.project20;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        ITelephone asadPhone;
+        asadPhone = new DeskPhone(90155394);
+        asadPhone.powerOn();
+        asadPhone.callPhone(90155394);
+        asadPhone.answer();
+
+        asadPhone = new MobilePhone(24565);
+        asadPhone.powerOn();
+        asadPhone.callPhone(24565);
+        asadPhone.answer();
+
+
+        }
+
+
     }
 
-}
+
